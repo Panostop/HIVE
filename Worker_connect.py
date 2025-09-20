f1c1627b0eb7c1213a00961fb751c9bfe72f180f
Panostop <94406359+Panostop@users.noreply.github.com>
2025-09-21 01:32:50 +0200

    création du fichier de bibliothèques

--- Worker_connect.py
+++ Worker_connect.py
@@ -8,6 +8,7 @@ import subprocess
 import threading
 import netifaces, struct #récupération du netmask
 import hashlib, base64 # encodage du mot de passe
+import funcs
 
 IDWorker = os.getlogin()  # ID du worker
 
@@ -21,29 +22,10 @@ socketEcoute.bind(('', 4173)) # prépa d'écoute sur toutes les adresses dispos 
 addrReine = None 
 isAdopted:bool = False
 
-def calcBroadcast():
-    global isAdopted, broadcastAddr
-    # ----- Calcul de l'adresse braoadcast ----- #
-    
-    socketEnvoi.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST) # activation du broadcast
-    
-    # récupération du netmask
-    iface = netifaces.gateways()['default'][netifaces.AF_INET][1]  # interface par défaut
-    netmask = netifaces.ifaddresses(iface)[netifaces.AF_INET][0]['netmask']
-    
-    # récupération de l'ip worker
-    IPworker = socket.gethostbyname(socket.gethostname()) 
-    
-    # calcul de l'adresse de broadcast
-    IPworker_int = struct.unpack("!I", socket.inet_aton(IPworker))[0] #conversions
-    netmask_int = struct.unpack("!I", socket.inet_aton(netmask))[0]   #
-
-    broadcast_int = IPworker_int | ~netmask_int & 0xFFFFFFFF
-    broadcastAddr = socket.inet_ntoa(struct.pack("!I", broadcast_int))
+broadcastAddr = funcs.calcBroadcast(socketEnvoi) # calcul de l'adresse broadcast
 
 def FREEPing():
     global isAdopted, broadcastAddr
-    calcBroadcast()
     
     # ----- Envoi du message ----- #
     while not isAdopted:
