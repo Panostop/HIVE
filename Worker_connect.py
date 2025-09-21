dd83f1aef51b98dc82dfd2caf7e38c1c49a5c11d
Panostop <94406359+Panostop@users.noreply.github.com>
2025-09-21 03:01:49 +0200

    amélioration de la librairie + éclairage du worker_connnect

--- Worker_connect.py
+++ Worker_connect.py
@@ -1,16 +1,13 @@
 '''
-Génération de la clé ssh à partir de l'ID du worker et du temps depuis epoch
+Protocole de connexion et de sécurisation du Worker
 '''
 from time import time
 import os
 import socket
 import subprocess
 import threading
-import netifaces, struct #récupération du netmask
-import hashlib, base64 # encodage du mot de passe
-import funcs
+import HIVELib
 
-IDWorker = os.getlogin()  # ID du worker
 
 # Création des sockets
 socketEcoute = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -18,11 +15,11 @@ socketEnvoi = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 socketEcoute.bind(('', 4173)) # prépa d'écoute sur toutes les adresses dispos sur le port HIVE
 
-#globalisation des variables
-addrReine = None 
+#déclaration des variables
+IDWorker = os.getlogin()  # ID du worker
+addrReine:str = None 
 isAdopted:bool = False
-
-broadcastAddr = funcs.calcBroadcast(socketEnvoi) # calcul de l'adresse broadcast
+broadcastAddr:str = HIVELib.calcBroadcast(socketEnvoi) # calcul de l'adresse broadcast
 
 def FREEPing():
     global isAdopted, broadcastAddr
@@ -32,35 +29,20 @@ def FREEPing():
         socketEnvoi.sendto(b"FREE", (broadcastAddr, 4173))
         time.sleep(60)
     
-def ADPTListen():
-    message = None
-    global addrReine, isAdopted
-    while message != b'ADPT':
-        message, addrReine = socketEcoute.recvfrom(1024) # buffer de 1024o lu non stop
-    isAdopted
-
-def generateurMDP():
-    # Génération de la clé ssh (10 caractères)
-    global password, IDWorker
-    
-    graine = int(time.time()) #graine
-    
-    #j'ai aucune idée de comment, mais ca fait ce que je veux
-    digest = hashlib.blake2s(f"{IDWorker}{graine}".encode()).digest()
-    password = base64.urlsafe_b64encode(digest).rstrip(b'=').decode()[:10]
 
 
+# si on a pas de busy.json (PARTIE MATHIAS), on ping 
 threadFREEPing = threading.Thread(target=FREEPing, daemon=True) # création du thread
-
-# si on a pas de busy.json, on ping
 threadFREEPing.start()
-ADPTListen() # on écoute en même temps
+
 # isAdopted = True -- on est adopté, le ping s'arrête
+isAdopted, addrReine = HIVELib.Listen(socketEcoute, "ADPT", addrReine)
+threadFREEPing.join() #on coupe le ping
 
 # on envoie un ack
 socketEnvoi.sendto(b"OK", (addrReine, 4173))
 # on génère le mot de passe
-generateurMDP()
+password = HIVELib.generateurMDP(IDWorker)
 time.sleep(1) 
 socketEnvoi.sendto(b"BUSY", (broadcastAddr, 4173)) #update tout le réseau
 
