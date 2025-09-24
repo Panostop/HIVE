ad4d8bc2ba8261f4ebc5ffd86f1ce22553e62eca
mathiasC <105153480+technovieux@users.noreply.github.com>
2025-09-24 17:41:07 +0200

    Update HIVELib.py
    j'ai ajouté une fonction pour modifier le json, ça modifie d'abord json_data avant de modifier le fichier JSON

--- HIVELib.py
+++ HIVELib.py
@@ -5,6 +5,18 @@ import socket
 from time import time
 import netifaces, struct #récupération du netmask
 
+json_data = {}
+
+# structure de base du json
+infos = {
+    "queen_MAC": "",
+    "queen_IP": "",
+    "last_updated": "",
+    "logs": [
+    ]
+}
+
+
 def calcBroadcast(socketBroadcast) -> str:
     # ----- Calcul de l'adresse braoadcast ----- #
     
@@ -36,4 +48,50 @@ def Listen(socketEcoute:socket) -> bool:
     message = None
     message, addrSource = socketEcoute.recvfrom(1024) # buffer de 1024o bloquant en attente de réception
     
+
     return message, addrSource
+
+
+
+
+class busyfile:
+    def init_data():
+        global json_data
+        json_data = busyfile.charger_json()
+
+    def charger_json():
+        if JSON_FILE.exists():
+            with open(JSON_FILE, "r", encoding="utf-8") as f:
+                return json.load(f)
+        return {}  # si fichier vide/inexistant
+
+    def sauvegarder_json(data):
+        with open(JSON_FILE, "w", encoding="utf-8") as f:
+            json.dump(data, f, indent=4, ensure_ascii=False)
+
+    def ajouter_log(message):
+        global json_data
+        timestamp = time.strftime('%d/%m/%Y-%H:%M:%S')
+        message = f"[{timestamp}] - {message}"
+        json_data.setdefault("logs", []).append(message)
+        busyfile.sauvegarder_json(json_data)
+
+    def create():
+        global json_data
+        if not JSON_FILE.exists():
+            with open(JSON_FILE, "w", encoding="utf-8") as f:
+                json.dump(infos, f, indent=4, ensure_ascii=False)
+                json_data = infos.copy()
+        busyfile.sauvegarder_json(json_data)
+
+    def modify(key, value):
+        global json_data
+        if key in infos:
+            json_data[key] = value
+            busyfile.sauvegarder_json(json_data)
+        else:
+            print(f"Clé '{key}' non trouvée dans le JSON.")
+
+    def update_time():
+        global json_data
+        busyfile.modify("last_updated", time.strftime('%d/%m/%Y-%H:%M:%S'))
